af3a41f03925d86f2dd9d2f004a7f00cfc770eeb
zhanghengjie <zhanghengjie@qianxin.com>
2020-08-06 11:15:55 +0800

    新增86题

--- 0086.partition/86.分隔链表.go
+++ 0086.partition/86.分隔链表.go
@@ -9,13 +9,29 @@ package problem0086
 // @lc code=start
 
 // ListNode Definition for singly-linked list.
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+// type ListNode struct {
+// 	Val  int
+// 	Next *ListNode
+// }
 
 func partition(head *ListNode, x int) *ListNode {
-
+	beforeHead := new(ListNode)
+	afterHead := new(ListNode)
+	before := beforeHead
+	after := afterHead
+	for head != nil {
+		if head.Val < x {
+			before.Next = head
+			before = before.Next
+		} else {
+			after.Next = head
+			after = after.Next
+		}
+		head = head.Next
+	}
+	before.Next = afterHead.Next
+	after.Next = nil
+	return beforeHead.Next
 }
 
 // @lc code=end
