3cdfac2eaf66994ddb498cca24fb7d783f0e8ba7
zhanghengjie <zhanghengjie@qianxin.com>
2020-08-14 19:50:39 +0800

    新增88题

--- 0088.merge/88.合并两个有序数组.go
+++ 0088.merge/88.合并两个有序数组.go
@@ -1,4 +1,5 @@
 package problem0088
+
 /*
  * @lc app=leetcode.cn id=88 lang=golang
  *
@@ -6,8 +7,34 @@ package problem0088
  */
 
 // @lc code=start
-func merge(nums1 []int, m int, nums2 []int, n int)  {
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	// 深度复制 nums1
+	temp := make([]int, m)
+	copy(temp, nums1)
 
+	j, k := 0, 0
+	for i := 0; i < len(nums1); i++ {
+		// nums2用完了
+		if k >= n {
+			nums1[i] = temp[j]
+			j++
+			continue
+		}
+		// temp 用完了
+		if j >= m {
+			nums1[i] = nums2[k]
+			k++
+			continue
+		}
+		// 比较后，放入
+		if temp[j] < nums2[k] {
+			nums1[i] = temp[j]
+			j++
+		} else {
+			nums1[i] = nums2[k]
+			k++
+		}
+	}
 }
-// @lc code=end
 
+// @lc code=end
--- expriment/case.go
+++ expriment/case.go
@@ -1,39 +1,12 @@
 package main
 
-// ListNode is
-type ListNode struct {
-	Val  int
-	Next *ListNode
-}
+import (
+	"fmt"
+	"strings"
+)
 
-func partition(head *ListNode, x int) *ListNode {
-	beforeHead := new(ListNode)
-	afterHead := new(ListNode)
-	before := beforeHead
-	after := afterHead
-	for head != nil {
-		if head.Val < x {
-			before.Next = head
-			before = before.Next
-		} else {
-			after.Next = head
-			after = after.Next
-		}
-		head = head.Next
-	}
-	before.Next = afterHead.Next
-	after.Next = nil
-	return beforeHead.Next
-}
 func main() {
-	first := new(ListNode)
-	second := new(ListNode)
-	tail := new(ListNode)
-	first.Val = 4
-	second.Val = 3
-	tail.Val = 2
-	first.Next = second
-	second.Next = tail
-	tail.Next = nil
-	partition(first, 3)
+	var s1 = []string{"hello", "liqing.io"}
+	var s2 = s1
+	fmt.Println("s2= ", strings.Join(s2, " "))
 }
