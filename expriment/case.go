316ab981538bf5fec0a01fe6cf8ad09a17c35f24
zhanghengjie <zhanghengjie@qianxin.com>
2021-02-25 16:10:26 +0800

    add 10

--- expriment/case.go
+++ expriment/case.go
@@ -4,31 +4,27 @@ import (
 	"fmt"
 )
 
-func countVowelStrings(n int) int {
+func isMatch(s string, p string) bool {
+	j := 0
 
-	dp := make([][]int, 5)
-	for i := range dp {
-		dp[i] = make([]int, n+1)
-	}
-	dp[0][1] = 1
-	dp[1][1] = 1
-	dp[2][1] = 1
-	dp[3][1] = 1
-	dp[4][1] = 1
-	for i := 2; i <= n; i++ {
-		for j := 0; j < 5; j++ {
-			sum := 0
-			for k := j; k < 5; k++ {
-				sum = sum + dp[k][i-1]
+	for i := 0; i < len(s); i++ {
+
+		if j >= len(p) {
+			return false
+		}
+
+		if (p[j] >= 65 && p[j] <= 90) || (p[j] >= 97 && p[j] <= 122) {
+			if p[j] != s[i] {
+				return false
 			}
-			dp[j][i] = sum
+			j++
+			continue
 		}
 	}
-	ans := dp[0][n] + dp[1][n] + dp[2][n] + dp[3][n] + dp[4][n]
-	return ans
+	return true
 }
 func main() {
 
-	ans := countVowelStrings(2)
-	fmt.Printf("the ans is:%d\n", ans)
+	ans := isMatch("aa", "a")
+	fmt.Printf("the ans is:%t\n", ans)
 }
