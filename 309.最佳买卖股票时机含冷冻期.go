657e221fde8c41ac227a5c9b287e3a31d2cc7c46
zhanghengjie <zhanghengjie@qianxin.com>
2022-03-24 19:55:04 +0800

    add 309

---
+++ 309.最佳买卖股票时机含冷冻期.go
@@ -0,0 +1,33 @@
+package leetcodego
+
+/*
+ * @lc app=leetcode.cn id=309 lang=golang
+ *
+ * [309] 最佳买卖股票时机含冷冻期
+ */
+
+// @lc code=start
+func maxProfit(prices []int) int {
+
+	if len(prices) == 0 {
+		return 0
+	}
+	n := len(prices)
+	f := make([][3]int, n)
+	f[0][0] = -prices[0]
+	for i := 1; i < n; i++ {
+		f[i][0] = max(f[i-1][0], f[i-1][2]-prices[i])
+		f[i][1] = f[i-1][0] + prices[i]
+		f[i][2] = max(f[i-1][1], f[i-1][2])
+	}
+	return max(f[n-1][1], f[n-1][2])
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
