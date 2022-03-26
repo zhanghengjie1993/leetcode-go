059b7f9f06f6a0bed08eb25cbd65ee85949eae45
zhanghj <1920515424@qq.com>
2022-03-26 21:36:18 +0800

    add 300

--- 300.最长递增子序列.go
+++ 300.最长递增子序列.go
@@ -9,6 +9,32 @@ package leetcodego
 // @lc code=start
 func lengthOfLIS(nums []int) int {
 
+	if len(nums) == 1 {
+		return 1
+	}
+	dp := make([]int, len(nums))
+	dp[0] = 1
+	temp := 0
+	ans := 0
+	for i := 1; i < len(nums); i++ {
+		for j := 0; j < i; j++ {
+			if nums[i] > nums[j] {
+				temp = dp[j] + 1
+			} else {
+				temp = 1
+			}
+			dp[i] = max(temp, dp[i])
+		}
+		ans = max(ans, dp[i])
+	}
+	return ans
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
 
 // @lc code=end
