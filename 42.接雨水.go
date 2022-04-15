f204e86b616f397fd1142b8e09b92c2a2d4306fb
zhanghengjie <zhanghengjie@qianxin.com>
2022-04-15 11:20:05 +0800

    add

---
+++ 42.接雨水.go
@@ -0,0 +1,21 @@
+package leetcodego
+
+/*
+ * @lc app=leetcode.cn id=42 lang=golang
+ *
+ * [42] 接雨水
+ */
+
+// @lc code=start
+func trap(height []int) int {
+
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// @lc code=end
